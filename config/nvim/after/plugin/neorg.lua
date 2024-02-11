da9eba2d5145b056f24dbb399ac412d288ef3a89
sij1nk <gergo.rajnavolgyi@gmail.com>
2024-02-11 15:43:32 +0100

    Add workaround for which-key #476

--- config/nvim/after/plugin/neorg.lua
+++ config/nvim/after/plugin/neorg.lua
@@ -17,3 +17,15 @@ neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, key
     noremap = true,
   })
 end)
+
+-- this is to fix bug: https://github.com/folke/which-key.nvim/issues/476
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "Set up neorg Which-Key descriptions",
+  group = vim.api.nvim_create_augroup("neorg_mapping_descriptions", { clear = true }),
+  pattern = "norg",
+  callback = function()
+    vim.keymap.set("n", "<localleader>", function()
+      require("which-key").show(",")
+    end, { buffer = true })
+  end,
+})
