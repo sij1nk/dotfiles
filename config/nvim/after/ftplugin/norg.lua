705cb38266875992112bc40c76acbf2bae326682
sij1nk <gergo.rajnavolgyi@gmail.com>
2024-03-03 07:50:52 +0100

    Add workaround to auto-close Neorg folds on file open

--- config/nvim/after/ftplugin/norg.lua
+++ config/nvim/after/ftplugin/norg.lua
@@ -1,2 +1,8 @@
 vim.o.textwidth = 80
 vim.o.conceallevel = 2
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = vim.api.nvim_create_augroup("NeorgAutoOpenFolds", {}),
+  pattern = { "*.norg" },
+  command = "normal zn<CR>",
+})
