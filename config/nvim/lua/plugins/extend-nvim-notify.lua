1fe8f79f098c05cd74f75f79cdcf602c36ace539
sij1nk <gergo.rajnavolgyi@gmail.com>
2024-07-03 20:17:53 +0200

    nvim: add keymap for showing notifications

--- config/nvim/lua/plugins/extend-nvim-notify.lua
+++ config/nvim/lua/plugins/extend-nvim-notify.lua
@@ -1,1 +1,20 @@
-return { "rcarriga/nvim-notify", enabled = true, opts = { render = "compact" } }
+return {
+  "rcarriga/nvim-notify",
+  enabled = true,
+  dependencies = { "nvim-telescope/telescope.nvim" },
+  opts = { render = "compact" },
+  keys = {
+    {
+      "<leader>uN",
+      function()
+        local notify = require("notify")
+        if vim.tbl_count(notify.history()) == 0 then
+          print("There are no notifications to show")
+          return
+        end
+        vim.cmd("Telescope notify")
+      end,
+      desc = "Show All Notifications",
+    },
+  },
+}
