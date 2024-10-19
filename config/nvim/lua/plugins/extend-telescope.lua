f59870b86a027092eec67189b8ab7174f5f45e4d
sij1nk <gergo.rajnavolgyi@gmail.com>
2024-10-19 10:50:34 +0200

    nvim: add telescope-emoji

--- config/nvim/lua/plugins/extend-telescope.lua
+++ config/nvim/lua/plugins/extend-telescope.lua
@@ -1,5 +1,8 @@
 return {
   "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "xiyaowong/telescope-emoji.nvim",
+  },
   keys = {
     {
       "<leader><space>",
@@ -9,4 +12,18 @@ return {
     { "<leader>ff", LazyVim.pick("auto", { follow = true }), desc = "Find Files (Root Dir)" },
     { "<leader>fF", LazyVim.pick("auto", { follow = true, root = false }), desc = "Find Files (cwd)" },
   },
+  config = function(opts)
+    opts = vim.tbl_extend("force", opts, {
+      extensions = {
+        emoji = {
+          action = function(emoji)
+            vim.api.nvim_put({ emoji.value }, "c", false, true)
+          end,
+        },
+      },
+    })
+    local telescope = require("telescope")
+    telescope.setup(opts)
+    telescope.load_extension("emoji")
+  end,
 }
