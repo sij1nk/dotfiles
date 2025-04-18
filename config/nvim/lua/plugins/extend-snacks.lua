48a04cd1506e90f5ff66f6761b98d73a9b69df42
sij1nk <gergo.rajnavolgyi@gmail.com>
2025-04-18 09:57:36 +0200

    nvim: canarify snacks picker

--- config/nvim/lua/plugins/extend-snacks.lua
+++ config/nvim/lua/plugins/extend-snacks.lua
@@ -1,11 +1,29 @@
-local function is_wide()
-  return vim.o.columns >= 120
-end
-
 return {
   "snacks.nvim",
   opts = {
     scroll = { enabled = false },
     indent = { animate = { enabled = false } },
+    picker = {
+      win = {
+        input = {
+          keys = {
+            ["n"] = "list_down",
+            ["e"] = "list_up",
+          },
+        },
+        list = {
+          keys = {
+            ["h"] = "focus_input",
+            ["n"] = "list_down",
+            ["e"] = "list_up",
+          },
+        },
+        preview = {
+          keys = {
+            ["h"] = "focus_input",
+          },
+        },
+      },
+    },
   },
 }
