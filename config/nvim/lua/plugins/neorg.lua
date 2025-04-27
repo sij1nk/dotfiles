b74db5dbc15db6fd0eabd765e26e7f0353d5b8e4
sij1nk <gergo.rajnavolgyi@gmail.com>
2025-04-27 10:22:55 +0200

    nvim: add follow link binding to neorg
    Previously this was <CR>, but now that's for saving the file. `gd` makes
    sense here

--- config/nvim/lua/plugins/neorg.lua
+++ config/nvim/lua/plugins/neorg.lua
@@ -28,6 +28,7 @@ return {
     keys = {
       { "<C-l>", "<cmd>Neorg roam node<cr>", desc = "List Neorg notes", mode = { "n", "i" }, ft = "norg" },
       { "<C-b>", "<cmd>Neorg roam block<cr>", desc = "List Neorg blocks", mode = { "n", "i" }, ft = "norg" },
+      { "gd", "<Plug>(neorg.esupports.hop.hop-link)", desc = "[neorg] Jump to Link", mode = "n", ft = "norg" },
     },
     config = function()
       local notes_path = env_or_default("NEORG_NOTES_WORKSPACE_DIR", "~/Notes")
