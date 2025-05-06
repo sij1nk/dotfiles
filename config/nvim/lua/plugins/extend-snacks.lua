3b05e1d56edb32507aafc5715e9ed0bd35c3ce86
sij1nk <gergo.rajnavolgyi@gmail.com>
2025-05-06 08:24:55 +0200

    nvim: add snacks.picker previewer scrolling and frecency matcher

--- config/nvim/lua/plugins/extend-snacks.lua
+++ config/nvim/lua/plugins/extend-snacks.lua
@@ -1,11 +1,22 @@
-local function is_wide()
-  return vim.o.columns >= 120
-end
-
 return {
-  "snacks.nvim",
+  "folke/snacks.nvim",
   opts = {
     scroll = { enabled = false },
     indent = { animate = { enabled = false } },
+    picker = {
+      matcher = {
+        frecency = true,
+      },
+      win = {
+        input = {
+          keys = {
+            ["J"] = { "preview_scroll_down", mode = { "n" } },
+            ["K"] = { "preview_scroll_up", mode = { "n" } },
+            ["H"] = { "preview_scroll_left", mode = { "n" } },
+            ["L"] = { "preview_scroll_right", mode = { "n" } },
+          },
+        },
+      },
+    },
   },
 }
