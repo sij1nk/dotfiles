145b4510883fcc65af158b2fecf01d61eed0d2de
sij1nk <gergo.rajnavolgyi@gmail.com>
2025-05-09 07:18:45 +0200

    nvim: use blink.cmp for cmdline and handle <Esc> correctly

--- config/nvim/lua/plugins/extend-blink.lua
+++ config/nvim/lua/plugins/extend-blink.lua
@@ -100,6 +100,39 @@ return {
       ["<C-j>"] = { "select_next", "fallback" },
       ["<C-k>"] = { "select_prev", "fallback" },
       ["<C-s>"] = { "show_signature", "hide_signature", "fallback" },
+      ["<ESC>"] = {
+        -- cancel completion and go back to Normal mode
+        function(cmp)
+          if cmp.is_visible() then
+            cmp.cancel()
+            -- Esc must be scheduled, to ensure that the cursor is in the correct buffer when the cancel (which is also scheduled) undos the cmp preview
+            vim.schedule(function()
+              vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-c>", true, true, true), "n", true)
+            end)
+            return true
+          end
+        end,
+        "fallback",
+      },
+    },
+    cmdline = {
+      enabled = true,
+      keymap = {
+        preset = "inherit",
+
+        ["<ESC>"] = {
+          -- cancel completion, but don't close cmdline
+          function(cmp)
+            if cmp.is_visible() then
+              cmp.cancel()
+            else
+              vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-c>", true, true, true), "n", true)
+            end
+          end,
+        },
+      },
+      completion = { menu = { auto_show = true } },
+      sources = { "path" },
     },
   },
 }
