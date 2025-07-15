32ca3566606c1069f01490e0b889dba08720f060
sij1nk <gergo.rajnavolgyi@gmail.com>
2025-07-15 07:49:36 +0200

    nvim: set filetype for new_bookmark file

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -4,17 +4,37 @@
 
 local theme_persistence = require("config.theme_persistence")
 
+local update_theme_augroup = vim.api.nvim_create_augroup("UpdateTheme", {})
+
 vim.api.nvim_create_autocmd("Signal", {
-  group = vim.api.nvim_create_augroup("UpdateTheme", {}),
+  group = update_theme_augroup,
   pattern = "SIGUSR1",
   callback = theme_persistence.update_theme_from_file,
 })
 
 vim.api.nvim_create_autocmd("BufWritePost", {
-  group = vim.api.nvim_create_augroup("UpdateTheme", {}),
+  group = update_theme_augroup,
   pattern = "nvim-theme",
   callback = function()
     theme_persistence.update_theme_from_file()
     theme_persistence.notify_nvim_instances()
+  end,
+})
+
+local create_bookmark_augroup = vim.api.nvim_create_augroup("CreateBookmark", {})
+
+vim.api.nvim_create_autocmd("BufUnload", {
+  group = create_bookmark_augroup,
+  pattern = "/tmp/new_bookmark",
+  callback = function()
+    vim.system({ "create-bookmark" })
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = create_bookmark_augroup,
+  pattern = "/tmp/new_bookmark",
+  callback = function()
+    vim.bo.filetype = "conf"
   end,
 })
