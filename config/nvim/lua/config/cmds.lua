e882d7ab9974ac486ffef21400f6ea8217278ff2
sij1nk <gergo.rajnavolgyi@gmail.com>
2026-03-22 08:22:52 +0100

    Add scripts + nvim Cppath command

--- config/nvim/lua/config/cmds.lua
+++ config/nvim/lua/config/cmds.lua
@@ -41,3 +41,9 @@ function NeorgInsertRnoteLink()
 end
 
 vim.api.nvim_create_user_command("NeorgInsertRnoteLink", NeorgInsertRnoteLink, {})
+
+vim.api.nvim_create_user_command("Cppath", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  vim.notify('Copied "' .. path .. '" to the clipboard!')
+end, {})
